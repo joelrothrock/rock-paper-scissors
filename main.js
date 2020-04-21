9ebd47205776bb826a8cc91913a68974cd64b738
joelrothrock <55121469+joelrothrock@users.noreply.github.com>
2020-04-20 19:39:23 -0500

    Initial save
    First draft, basic HTML bones, limited CSS, JS outline

---
+++ main.js
@@ -0,0 +1,11 @@
+// give player one three options
+
+// activate that choice on click
+
+// when they chose the cpu randomly selects an option as well
+
+// decide whether it is a win, loss, or tie
+
+// update score based on the outcome
+
+// update the round at the end of each round
